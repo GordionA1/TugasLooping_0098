3f3c5436416adf70a9d320940ae095d4fd6791fa
GordionA1 <farisazka2506@gmail.com>
2026-04-01 09:44:28 +0700

    membuat fungsi looping dan pengecekan kondisi/if

--- TugasLooping.cpp
+++ TugasLooping.cpp
@@ -22,5 +22,11 @@ bool isPrima(int n) {
 // Function untuk cek bilangan Fibonacci
 bool isFibonacci(int n){
      int a = 0, b = 1, c = 0;
+      while (c <= n) {
+        if (c == n) {
+            return true;
+        }
+    }
+    return false;
 }
 
